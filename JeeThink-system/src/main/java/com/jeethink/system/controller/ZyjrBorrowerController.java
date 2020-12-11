2fde7303e49e51e0c4f38eb5e7c3281dbf3af5ff
chsdl <chsdlqwd123>
2020-12-11 15:04:11 +0800

    1

--- JeeThink-system/src/main/java/com/jeethink/system/controller/ZyjrBorrowerController.java
+++ JeeThink-system/src/main/java/com/jeethink/system/controller/ZyjrBorrowerController.java
@@ -1,8 +1,6 @@
 package com.jeethink.system.controller;
 
 import java.util.List;
-import java.util.Map;
-
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,12 +15,8 @@ import com.jeethink.common.annotation.Log;
 import com.jeethink.common.core.controller.BaseController;
 import com.jeethink.common.core.domain.AjaxResult;
 import com.jeethink.common.enums.BusinessType;
-import com.jeethink.system.domain.ZyjrBusiness;
-import com.jeethink.system.service.IZyjrBusinessService;
-
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-
+import com.jeethink.system.domain.ZyjrBorrower;
+import com.jeethink.system.service.IZyjrBorrowerService;
 import com.jeethink.common.utils.poi.ExcelUtil;
 import com.jeethink.common.core.page.TableDataInfo;
 
@@ -30,90 +24,80 @@ import com.jeethink.common.core.page.TableDataInfo;
  * 【请填写功能名称】Controller
  * 
  * @author jeethink
- * @date 2020-12-09
+ * @date 2020-12-10
  */
 @RestController
-@RequestMapping("/system/business")
-@Api("zzzzzzzzzz")
-public class ZyjrBusinessController extends BaseController
+@RequestMapping("/system/borrower")
+public class ZyjrBorrowerController extends BaseController
 {
     @Autowired
-    private IZyjrBusinessService zyjrBusinessService;
+    private IZyjrBorrowerService zyjrBorrowerService;
 
     /**
      * 查询【请填写功能名称】列表
      */
-    @PreAuthorize("@ss.hasPermi('process:business:list')")
+    @PreAuthorize("@ss.hasPermi('system:borrower:list')")
     @GetMapping("/list")
-    @ApiOperation("查询参数列表")
-    public TableDataInfo list(ZyjrBusiness zyjrBusiness)
+    public TableDataInfo list(ZyjrBorrower zyjrBorrower)
     {
         startPage();
-        List<ZyjrBusiness> list = zyjrBusinessService.selectZyjrBusinessList(zyjrBusiness);
+        List<ZyjrBorrower> list = zyjrBorrowerService.selectZyjrBorrowerList(zyjrBorrower);
         return getDataTable(list);
-    }
-    
-    @GetMapping("/info")
-    @ApiOperation("查询参数列表")
-    public AjaxResult info(String id)
-    {
-        Map<String,Object> list = zyjrBusinessService.seleMap(id);
-        return AjaxResult.success(list);
     }
 
     /**
      * 导出【请填写功能名称】列表
      */
-    @PreAuthorize("@ss.hasPermi('process:business:export')")
+    @PreAuthorize("@ss.hasPermi('system:borrower:export')")
     @Log(title = "【请填写功能名称】", businessType = BusinessType.EXPORT)
     @GetMapping("/export")
-    public AjaxResult export(ZyjrBusiness zyjrBusiness)
+    public AjaxResult export(ZyjrBorrower zyjrBorrower)
     {
-        List<ZyjrBusiness> list = zyjrBusinessService.selectZyjrBusinessList(zyjrBusiness);
-        ExcelUtil<ZyjrBusiness> util = new ExcelUtil<ZyjrBusiness>(ZyjrBusiness.class);
-        return util.exportExcel(list, "business");
+        List<ZyjrBorrower> list = zyjrBorrowerService.selectZyjrBorrowerList(zyjrBorrower);
+        ExcelUtil<ZyjrBorrower> util = new ExcelUtil<ZyjrBorrower>(ZyjrBorrower.class);
+        return util.exportExcel(list, "borrower");
     }
 
     /**
      * 获取【请填写功能名称】详细信息
      */
-    @PreAuthorize("@ss.hasPermi('process:business:query')")
+    @PreAuthorize("@ss.hasPermi('system:borrower:query')")
     @GetMapping(value = "/{id}")
-    public AjaxResult getInfo(@PathVariable("id") Long id)
+    public AjaxResult getInfo(@PathVariable("id") Integer id)
     {
-        return AjaxResult.success(zyjrBusinessService.selectZyjrBusinessById(id));
+        return AjaxResult.success(zyjrBorrowerService.selectZyjrBorrowerById(id));
     }
 
     /**
      * 新增【请填写功能名称】
      */
-    @PreAuthorize("@ss.hasPermi('process:business:add')")
+    @PreAuthorize("@ss.hasPermi('system:borrower:add')")
     @Log(title = "【请填写功能名称】", businessType = BusinessType.INSERT)
     @PostMapping
-    public AjaxResult add(@RequestBody ZyjrBusiness zyjrBusiness)
+    public AjaxResult add(@RequestBody ZyjrBorrower zyjrBorrower)
     {
-        return toAjax(zyjrBusinessService.insertZyjrBusiness(zyjrBusiness));
+        return toAjax(zyjrBorrowerService.insertZyjrBorrower(zyjrBorrower));
     }
 
     /**
      * 修改【请填写功能名称】
      */
-    @PreAuthorize("@ss.hasPermi('process:business:edit')")
+    @PreAuthorize("@ss.hasPermi('system:borrower:edit')")
     @Log(title = "【请填写功能名称】", businessType = BusinessType.UPDATE)
     @PutMapping
-    public AjaxResult edit(@RequestBody ZyjrBusiness zyjrBusiness)
+    public AjaxResult edit(@RequestBody ZyjrBorrower zyjrBorrower)
     {
-        return toAjax(zyjrBusinessService.updateZyjrBusiness(zyjrBusiness));
+        return toAjax(zyjrBorrowerService.updateZyjrBorrower(zyjrBorrower));
     }
 
     /**
      * 删除【请填写功能名称】
      */
-    @PreAuthorize("@ss.hasPermi('process:business:remove')")
+    @PreAuthorize("@ss.hasPermi('system:borrower:remove')")
     @Log(title = "【请填写功能名称】", businessType = BusinessType.DELETE)
 	@DeleteMapping("/{ids}")
-    public AjaxResult remove(@PathVariable Long[] ids)
+    public AjaxResult remove(@PathVariable Integer[] ids)
     {
-        return toAjax(zyjrBusinessService.deleteZyjrBusinessByIds(ids));
+        return toAjax(zyjrBorrowerService.deleteZyjrBorrowerByIds(ids));
     }
 }
--- JeeThink-system/src/main/java/com/jeethink/system/controller/ZyjrGuaranteeController.java
+++ JeeThink-system/src/main/java/com/jeethink/system/controller/ZyjrGuaranteeController.java
@@ -1,8 +1,6 @@
 package com.jeethink.system.controller;
 
 import java.util.List;
-import java.util.Map;
-
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,12 +15,8 @@ import com.jeethink.common.annotation.Log;
 import com.jeethink.common.core.controller.BaseController;
 import com.jeethink.common.core.domain.AjaxResult;
 import com.jeethink.common.enums.BusinessType;
-import com.jeethink.system.domain.ZyjrBusiness;
-import com.jeethink.system.service.IZyjrBusinessService;
-
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-
+import com.jeethink.system.domain.ZyjrGuarantee;
+import com.jeethink.system.service.IZyjrGuaranteeService;
 import com.jeethink.common.utils.poi.ExcelUtil;
 import com.jeethink.common.core.page.TableDataInfo;
 
@@ -30,90 +24,80 @@ import com.jeethink.common.core.page.TableDataInfo;
  * 【请填写功能名称】Controller
  * 
  * @author jeethink
- * @date 2020-12-09
+ * @date 2020-12-10
  */
 @RestController
-@RequestMapping("/system/business")
-@Api("zzzzzzzzzz")
-public class ZyjrBusinessController extends BaseController
+@RequestMapping("/system/guarantee")
+public class ZyjrGuaranteeController extends BaseController
 {
     @Autowired
-    private IZyjrBusinessService zyjrBusinessService;
+    private IZyjrGuaranteeService zyjrGuaranteeService;
 
     /**
      * 查询【请填写功能名称】列表
      */
-    @PreAuthorize("@ss.hasPermi('process:business:list')")
+    @PreAuthorize("@ss.hasPermi('system:guarantee:list')")
     @GetMapping("/list")
-    @ApiOperation("查询参数列表")
-    public TableDataInfo list(ZyjrBusiness zyjrBusiness)
+    public TableDataInfo list(ZyjrGuarantee zyjrGuarantee)
     {
         startPage();
-        List<ZyjrBusiness> list = zyjrBusinessService.selectZyjrBusinessList(zyjrBusiness);
+        List<ZyjrGuarantee> list = zyjrGuaranteeService.selectZyjrGuaranteeList(zyjrGuarantee);
         return getDataTable(list);
-    }
-    
-    @GetMapping("/info")
-    @ApiOperation("查询参数列表")
-    public AjaxResult info(String id)
-    {
-        Map<String,Object> list = zyjrBusinessService.seleMap(id);
-        return AjaxResult.success(list);
     }
 
     /**
      * 导出【请填写功能名称】列表
      */
-    @PreAuthorize("@ss.hasPermi('process:business:export')")
+    @PreAuthorize("@ss.hasPermi('system:guarantee:export')")
     @Log(title = "【请填写功能名称】", businessType = BusinessType.EXPORT)
     @GetMapping("/export")
-    public AjaxResult export(ZyjrBusiness zyjrBusiness)
+    public AjaxResult export(ZyjrGuarantee zyjrGuarantee)
     {
-        List<ZyjrBusiness> list = zyjrBusinessService.selectZyjrBusinessList(zyjrBusiness);
-        ExcelUtil<ZyjrBusiness> util = new ExcelUtil<ZyjrBusiness>(ZyjrBusiness.class);
-        return util.exportExcel(list, "business");
+        List<ZyjrGuarantee> list = zyjrGuaranteeService.selectZyjrGuaranteeList(zyjrGuarantee);
+        ExcelUtil<ZyjrGuarantee> util = new ExcelUtil<ZyjrGuarantee>(ZyjrGuarantee.class);
+        return util.exportExcel(list, "guarantee");
     }
 
     /**
      * 获取【请填写功能名称】详细信息
      */
-    @PreAuthorize("@ss.hasPermi('process:business:query')")
+    @PreAuthorize("@ss.hasPermi('system:guarantee:query')")
     @GetMapping(value = "/{id}")
-    public AjaxResult getInfo(@PathVariable("id") Long id)
+    public AjaxResult getInfo(@PathVariable("id") Integer id)
     {
-        return AjaxResult.success(zyjrBusinessService.selectZyjrBusinessById(id));
+        return AjaxResult.success(zyjrGuaranteeService.selectZyjrGuaranteeById(id));
     }
 
     /**
      * 新增【请填写功能名称】
      */
-    @PreAuthorize("@ss.hasPermi('process:business:add')")
+    @PreAuthorize("@ss.hasPermi('system:guarantee:add')")
     @Log(title = "【请填写功能名称】", businessType = BusinessType.INSERT)
     @PostMapping
-    public AjaxResult add(@RequestBody ZyjrBusiness zyjrBusiness)
+    public AjaxResult add(@RequestBody ZyjrGuarantee zyjrGuarantee)
     {
-        return toAjax(zyjrBusinessService.insertZyjrBusiness(zyjrBusiness));
+        return toAjax(zyjrGuaranteeService.insertZyjrGuarantee(zyjrGuarantee));
     }
 
     /**
      * 修改【请填写功能名称】
      */
-    @PreAuthorize("@ss.hasPermi('process:business:edit')")
+    @PreAuthorize("@ss.hasPermi('system:guarantee:edit')")
     @Log(title = "【请填写功能名称】", businessType = BusinessType.UPDATE)
     @PutMapping
-    public AjaxResult edit(@RequestBody ZyjrBusiness zyjrBusiness)
+    public AjaxResult edit(@RequestBody ZyjrGuarantee zyjrGuarantee)
     {
-        return toAjax(zyjrBusinessService.updateZyjrBusiness(zyjrBusiness));
+        return toAjax(zyjrGuaranteeService.updateZyjrGuarantee(zyjrGuarantee));
     }
 
     /**
      * 删除【请填写功能名称】
      */
-    @PreAuthorize("@ss.hasPermi('process:business:remove')")
+    @PreAuthorize("@ss.hasPermi('system:guarantee:remove')")
     @Log(title = "【请填写功能名称】", businessType = BusinessType.DELETE)
 	@DeleteMapping("/{ids}")
-    public AjaxResult remove(@PathVariable Long[] ids)
+    public AjaxResult remove(@PathVariable Integer[] ids)
     {
-        return toAjax(zyjrBusinessService.deleteZyjrBusinessByIds(ids));
+        return toAjax(zyjrGuaranteeService.deleteZyjrGuaranteeByIds(ids));
     }
 }
