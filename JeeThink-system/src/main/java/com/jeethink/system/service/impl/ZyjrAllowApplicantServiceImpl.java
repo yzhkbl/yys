d9dbdb74240d12147747b4241f129ebfefd95c9d
chsdl <chsdlqwd123>
2021-02-04 09:49:10 +0800

    1

--- JeeThink-system/src/main/java/com/jeethink/system/service/impl/ZyjrAllowApplicantServiceImpl.java
+++ JeeThink-system/src/main/java/com/jeethink/system/service/impl/ZyjrAllowApplicantServiceImpl.java
@@ -14,12 +14,12 @@ import com.alibaba.fastjson.JSON;
 
 /**
  * 【请填写功能名称】Service业务层处理
- * 
+ *
  * @author jeethink
  * @date 2020-12-22
  */
 @Service
-public class ZyjrAllowApplicantServiceImpl implements IZyjrAllowApplicantService 
+public class ZyjrAllowApplicantServiceImpl implements IZyjrAllowApplicantService
 {
     @Autowired
     private ZyjrAllowApplicantMapper zyjrAllowApplicantMapper;
@@ -28,7 +28,7 @@ public class ZyjrAllowApplicantServiceImpl implements IZyjrAllowApplicantService
 
     /**
      * 查询【请填写功能名称】
-     * 
+     *
      * @param userId 【请填写功能名称】ID
      * @return 【请填写功能名称】
      */
@@ -36,7 +36,7 @@ public class ZyjrAllowApplicantServiceImpl implements IZyjrAllowApplicantService
     public ZyjrAllowApplicant selectZyjrAllowApplicantById(Long userId, String transactionCode)
     {
         ZyjrAllowApplicant zyjrAllowApplicant = zyjrAllowApplicantMapper.selectZyjrAllowApplicantById(userId, transactionCode);
-        if(zyjrAllowApplicant.getIndustry()!=null){
+        if(zyjrAllowApplicant!=null&&zyjrAllowApplicant.getIndustry()!=null){
             ZyjrCard a=zyjrCardMapper.selectZyjrCardByTransactionCode("1");
             String str= a.getAlmebno();
             HashMap hashMap = JSON.parseObject(str, HashMap.class);
@@ -48,7 +48,7 @@ public class ZyjrAllowApplicantServiceImpl implements IZyjrAllowApplicantService
 
     /**
      * 查询【请填写功能名称】列表
-     * 
+     *
      * @param zyjrAllowApplicant 【请填写功能名称】
      * @return 【请填写功能名称】
      */
@@ -60,7 +60,7 @@ public class ZyjrAllowApplicantServiceImpl implements IZyjrAllowApplicantService
 
     /**
      * 新增【请填写功能名称】
-     * 
+     *
      * @param zyjrAllowApplicant 【请填写功能名称】
      * @return 结果
      */
@@ -75,7 +75,7 @@ public class ZyjrAllowApplicantServiceImpl implements IZyjrAllowApplicantService
 
     /**
      * 修改【请填写功能名称】
-     * 
+     *
      * @param zyjrAllowApplicant 【请填写功能名称】
      * @return 结果
      */
@@ -87,7 +87,7 @@ public class ZyjrAllowApplicantServiceImpl implements IZyjrAllowApplicantService
 
     /**
      * 批量删除【请填写功能名称】
-     * 
+     *
      * @param ids 需要删除的【请填写功能名称】ID
      * @return 结果
      */
@@ -99,7 +99,7 @@ public class ZyjrAllowApplicantServiceImpl implements IZyjrAllowApplicantService
 
     /**
      * 删除【请填写功能名称】信息
-     * 
+     *
      * @param id 【请填写功能名称】ID
      * @return 结果
      */
